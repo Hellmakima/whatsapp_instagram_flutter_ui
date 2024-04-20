9a0196621fe928e8ac9a16340d93464fbd0044de
Hellmakima <154533744+Hellmakima@users.noreply.github.com>
2024-04-20 22:42:26 +0530

    Update reels.dart

--- reels.dart
+++ reels.dart
@@ -1,77 +1,35 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:video_player/video_player.dart';
+import 'package:kk/post.dart';
 import 'colors.dart';
 
-class ReelsPage extends StatefulWidget {
+List<List<String>> posts = [
+  ["joshi.NIM", "new Painting", "color.mp4", "puff.jpg"],
+  ["ahijeetPOW", "sleepy Head Sarya", "v2.mp4", "surp.jpg"],
+  ["tf_matters", "the boys having fun", "v1.mp4", "smile.jpg"]
+];
+
+class ReelsPage extends StatelessWidget {
   const ReelsPage({super.key});
 
   @override
-  State<ReelsPage> createState() => _ReelsPageState();
-}
-
-class _ReelsPageState extends State<ReelsPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) =>
-          MyVideo(asset: 'v1.mp4')
-        )
-    );
-  }
-}
-
-class MyVideo extends StatefulWidget {
-  final String asset;
-  const MyVideo({super.key, required this.asset});
-
-  @override
-  State<MyVideo> createState() => _VideoState();
-
-}
-
-class _VideoState extends State<MyVideo> {
-  VideoPlayerController? _controller;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller = VideoPlayerController.asset('assets/${widget.asset}');
-    _controller?.initialize().then((_) => {
-      _controller?.play(),
-      _controller?.setLooping(true),
-      _controller?.setVolume(0),
-      setState(() { })
-    });
-  }
-  
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _controller?.dispose();
-    super.dispose();
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: _controller!.value.isInitialized
-              ? AspectRatio(
-            aspectRatio: _controller!.value.aspectRatio,
-            // aspectRatio: 0.75,
-            child: VideoPlayer(_controller!),
-          )
-          : CircularProgressIndicator(),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        color: darkGrey,
+        child: ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) =>
+                // Text(posts[index][0], style: TextStyle(color: lighterGrey),)
+              Post(
+                user: posts[index][0],
+                caption: posts[index][1],
+                postAsset: posts[index][2],
+                profileAsset: posts[index][3],
+              )
         ),
-        SizedBox(height: 1100,)
-      ],
+      ),
     );
   }
 }
-
